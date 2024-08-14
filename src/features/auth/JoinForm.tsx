e2d1575a45abd86bfef1087b25cc01a0dd9f8e4d
marcovidali2 <marcovidali6@proton.me>
2024-08-14 15:47:23 +0200

    feat(app): Implement emojis

--- src/features/auth/JoinForm.tsx
+++ src/features/auth/JoinForm.tsx
@@ -1,14 +1,12 @@
-import { HiEnvelope } from "react-icons/hi2";
-
 const JoinForm = () => {
     return (
         <div>
             <div className="card bg-neutral text-neutral-content w-96">
                 <form className="card-body items-center text-center space-y-2">
-                    <h1 className="card-title">Join Fastcode</h1>
+                    <h1 className="card-title">Start using Fastcode 🚀</h1>
 
                     <label className="input input-bordered flex items-center gap-2 w-full">
-                        <HiEnvelope />
+                        <span>✉️</span>
                         <input
                             type="email"
                             className="grow"
@@ -16,7 +14,7 @@ const JoinForm = () => {
                         />
                     </label>
 
-                    <button className="btn btn-primary w-full">Join!</button>
+                    <button className="btn btn-primary w-full">Join</button>
                 </form>
             </div>
         </div>
