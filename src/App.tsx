3635b9c1cd12a4c75337da4f47503811ae44f908
marcovidali2 <marcovidali6@proton.me>
2024-08-18 12:11:01 +0200

    feat(app): Add toast notifications

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import AppLayout from "./ui/AppLayout";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient();
 
@@ -13,24 +14,30 @@ const Join = lazy(() => import("./pages/Join"));
 
 const App = () => {
     return (
-        <QueryClientProvider client={queryClient}>
-            <Suspense
-                fallback={
-                    <AbsoluteCenter>
-                        <Spinner />
-                    </AbsoluteCenter>
-                }
-            >
-                <BrowserRouter>
-                    <Routes>
-                        <Route element={<AppLayout />}>
-                            <Route index element={<Home />} />
-                            <Route path="/join" element={<Join />} />
-                        </Route>
-                    </Routes>
-                </BrowserRouter>
-            </Suspense>
-        </QueryClientProvider>
+        <>
+            <Toaster
+                toastOptions={{ className: "bg-base-200 text-base-content" }}
+            />
+
+            <QueryClientProvider client={queryClient}>
+                <Suspense
+                    fallback={
+                        <AbsoluteCenter>
+                            <Spinner />
+                        </AbsoluteCenter>
+                    }
+                >
+                    <BrowserRouter>
+                        <Routes>
+                            <Route element={<AppLayout />}>
+                                <Route index element={<Home />} />
+                                <Route path="/join" element={<Join />} />
+                            </Route>
+                        </Routes>
+                    </BrowserRouter>
+                </Suspense>
+            </QueryClientProvider>
+        </>
     );
 };
 
--- src/features/auth/useJoin.ts
+++ src/features/auth/useJoin.ts
@@ -1,9 +1,13 @@
+import toast from "react-hot-toast";
+
 import { useMutation } from "@tanstack/react-query";
 import { join as apiJoin } from "../../services/apiAuth";
 
 export function useJoin() {
     const { mutate: join, isPending } = useMutation({
         mutationFn: (email: string) => apiJoin(email),
+        onSuccess: () => toast.success("A verification email was sent to you"),
+        onError: () => toast.error("An error has occurred while joining"),
     });
 
     return { join, isPending };
