73e7738c7d00f9582918d22169d962dffbb4c6c1
marcovidali2 <marcovidali6@proton.me>
2024-08-21 14:25:23 +0200

    fix(app): Full screen

--- src/ui/AbsoluteCenter.tsx
+++ src/ui/AbsoluteCenter.tsx
@@ -2,9 +2,7 @@ import { PropsWithChildren } from "react";
 
 const AbsoluteCenter = ({ children }: PropsWithChildren) => {
     return (
-        <div className="h-full flex justify-center items-center">
-            {children}
-        </div>
+        <div className="h-dvh flex justify-center items-center">{children}</div>
     );
 };
 
--- src/ui/AppLayout.tsx
+++ src/ui/AppLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 const AppLayout = () => {
     return (
-        <div className="h-dvh p-4">
+        <div className="p-4">
             <Outlet />
         </div>
     );
